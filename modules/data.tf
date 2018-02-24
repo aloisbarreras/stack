95211412758b1ddadd87784887a75d7a3e4774d1
Alois Barreras <aloisbarreras@icloud.com>
2018-02-23 20:58:30 -0500

    inital commit for creating vpc

---
+++ modules/data.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "nat_ami" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-vpc-nat*"]
+  }
+}

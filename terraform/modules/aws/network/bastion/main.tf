7ef44aa8219cb25a98af3ede707c4a981fb97701
Alois Barreras <aloisbarreras@icloud.com>
2018-02-27 22:07:02 -0500

    removed unused var

--- terraform/modules/aws/network/bastion/main.tf
+++ terraform/modules/aws/network/bastion/main.tf
@@ -14,10 +14,6 @@ variable "instance_type" {
   description = "Instance type, see a list at: https://aws.amazon.com/ec2/instance-types/"
 }
 
-variable "region" {
-  description = "AWS Region, e.g us-east-1"
-}
-
 variable "security_groups" {
   description = "a comma separated string of security group IDs"
 }

2e99b4087ca755ac1848497d56a484bd3d16ba2e
Alois Barreras <aloisbarreras@icloud.com>
2018-03-08 13:57:25 -0500

    added rds module

---
+++ terraform/modules/aws/data/rds/main.tf
@@ -0,0 +1,75 @@
+variable "name" {}
+variable "environment" {}
+variable "database_name" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "username" {}
+variable "password" {}
+
+variable "storage_type" {
+  default = "gp2"
+}
+
+variable "allocated_storage" {
+  default = 50
+}
+
+variable "instance_class" {
+  default = "db.t2.micro"
+}
+
+variable "publicly_accessible" {
+  default = false
+}
+
+variable "skip_final_snapshot" {
+  default = true
+}
+
+variable "subnets" {
+  type = "list"
+}
+
+variable "security_groups" {
+  type = "list"
+}
+
+locals {
+  db_subnet_group_name = "${var.name}-subnet-group"
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "${local.db_subnet_group_name}"
+  subnet_ids = ["${var.subnets}"]
+
+  tags {
+    Name = "${local.db_subnet_group_name}"
+  }
+}
+
+resource "aws_db_instance" "main" {
+  identifier             = "${var.name}-${var.engine}"
+  allocated_storage      = "${var.allocated_storage}"
+  storage_type           = "${var.storage_type}"
+  engine                 = "${var.engine}"
+  engine_version         = "${var.engine_version}"
+  instance_class         = "${var.instance_class}"
+  name                   = "${var.database_name}"
+  username               = "${var.username}"
+  password               = "${var.password}"
+  db_subnet_group_name   = "${local.db_subnet_group_name}"
+  vpc_security_group_ids = ["${var.security_groups}"]
+  publicly_accessible    = "${var.publicly_accessible}"
+  skip_final_snapshot    = "${var.skip_final_snapshot}"
+
+  depends_on = ["aws_db_subnet_group.main"]
+
+  tags {
+    Name        = "${var.name}-postgres-${var.engine_version}"
+    environment = "${var.environment}"
+  }
+}
+
+output "endpoint" {
+  value = "${aws_db_instance.main.endpoint}"
+}
